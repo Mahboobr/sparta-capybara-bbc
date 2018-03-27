05715672b91bae9be389956784c9655c480e9fb8
Tech-A28 <tech-a28@Tech-A28s-MacBook-Air.local>
2018-03-27 23:01:16 +0100

    SecondCommit

--- lib/pages/bbc_homepage.rb
+++ lib/pages/bbc_homepage.rb
@@ -5,7 +5,6 @@ class BBC_Homepage
 
   HOMEPAGE_URL = "https://www.bbc.co.uk/"
   SIGN_IN_LINK_ID = "#idcta-username"
-  # SIGN_IN_LINK_ID = "#idcta-link"
 
   def visit_home
     visit(HOMEPAGE_URL)
@@ -16,7 +15,6 @@ class BBC_Homepage
   end
 
   def click_sign_in
-    # sign_in_link.click
     find(SIGN_IN_LINK_ID).click
   end
 end
--- spec/bbc_login_error_spec.rb
+++ spec/bbc_login_error_spec.rb
@@ -9,65 +9,65 @@ describe "testing the BBC registration journey" do
   ERR_WRONG_DATE_5 = "Oops, that day and month don't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
 
   context 'tests on date page' do
-  #   before(:each) do
-  #     @bbc_site = BBC_Site.new
-  #     @bbc_site.bbc_homepage.visit_home
-  #     @bbc_site.bbc_homepage.click_sign_in
-  #     @bbc_site.bbc_sign_up.click_register_link
-  #     @bbc_site.bbc_sign_up.click_over_13
-  #   end
-  #   context 'letters entered into fields' do
-  #     it "should throw error if day, month, and year fields are left blank" do
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
-  #     end
-  #     it "should throw error if letter is entered in day field, and month and year fields left blank" do
-  #       @bbc_site.bbc_sign_up.enter_day("a")
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
-  #     end
-  #     it "should throw error if letter is entered in day field and month field, and year field left blank" do
-  #       @bbc_site.bbc_sign_up.enter_day("a")
-  #       @bbc_site.bbc_sign_up.enter_month("b")
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
-  #     end
-  #     it "should throw error if letter is entered in day, month, and year fields" do
-  #       @bbc_site.bbc_sign_up.enter_day("a")
-  #       @bbc_site.bbc_sign_up.enter_month("b")
-  #       @bbc_site.bbc_sign_up.enter_year("c")
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
-  #     end
-  #   end
-  #   context 'valid inputs entered into fields with other empty fields remaining' do
-  #     it "should throw error if day has valid input, but month and year fields are left blank" do
-  #       @bbc_site.bbc_sign_up.enter_day("1")
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_2)
-  #     end
-  #     it "should throw error if month has a valid input, but day and year fields are left blank" do
-  #       @bbc_site.bbc_sign_up.enter_month("2")
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_4)
-  #     end
-  #     it "should throw error if year has a valid input, but day and month fields are left blank" do
-  #       @bbc_site.bbc_sign_up.enter_year("1990")
-  #       @bbc_site.bbc_sign_up.submit_button
-  #       expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_5)
-  #     end
-  #   end
-  #   context 'valid inputs entered into all fields' do
-  #     it "should not throw error if all fields have valid input" do
-  #       @bbc_site.bbc_sign_up.enter_day("1")
-  #       expect(@bbc_site.bbc_sign_up.get_day_value).to be_between(1, 31).inclusive
-  #       @bbc_site.bbc_sign_up.enter_month("2")
-  #       expect(@bbc_site.bbc_sign_up.get_month_value).to be_between(1, 12).inclusive
-  #       @bbc_site.bbc_sign_up.enter_year("1990")
-  #       expect(@bbc_site.bbc_sign_up.get_year_value.length).to eq(4)
-  #       @bbc_site.bbc_sign_up.submit_button
-  #     end
-  #   end
+    before(:each) do
+      @bbc_site = BBC_Site.new
+      @bbc_site.bbc_homepage.visit_home
+      @bbc_site.bbc_homepage.click_sign_in
+      @bbc_site.bbc_sign_up.click_register_link
+      @bbc_site.bbc_sign_up.click_over_13
+    end
+    context 'letters entered into fields' do
+      it "should throw error if day, month, and year fields are left blank" do
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
+      end
+      it "should throw error if letter is entered in day field, and month and year fields left blank" do
+        @bbc_site.bbc_sign_up.enter_day("a")
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
+      end
+      it "should throw error if letter is entered in day field and month field, and year field left blank" do
+        @bbc_site.bbc_sign_up.enter_day("a")
+        @bbc_site.bbc_sign_up.enter_month("b")
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
+      end
+      it "should throw error if letter is entered in day, month, and year fields" do
+        @bbc_site.bbc_sign_up.enter_day("a")
+        @bbc_site.bbc_sign_up.enter_month("b")
+        @bbc_site.bbc_sign_up.enter_year("c")
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_1)
+      end
+    end
+    context 'valid inputs entered into fields with other empty fields remaining' do
+      it "should throw error if day has valid input, but month and year fields are left blank" do
+        @bbc_site.bbc_sign_up.enter_day("1")
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_2)
+      end
+      it "should throw error if month has a valid input, but day and year fields are left blank" do
+        @bbc_site.bbc_sign_up.enter_month("2")
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_4)
+      end
+      it "should throw error if year has a valid input, but day and month fields are left blank" do
+        @bbc_site.bbc_sign_up.enter_year("1990")
+        @bbc_site.bbc_sign_up.submit_button
+        expect(@bbc_site.bbc_sign_up.date_error).to eq(ERR_WRONG_DATE_5)
+      end
+    end
+    context 'valid inputs entered into all fields' do
+      it "should not throw error if all fields have valid input" do
+        @bbc_site.bbc_sign_up.enter_day("1")
+        expect(@bbc_site.bbc_sign_up.get_day_value).to be_between(1, 31).inclusive
+        @bbc_site.bbc_sign_up.enter_month("2")
+        expect(@bbc_site.bbc_sign_up.get_month_value).to be_between(1, 12).inclusive
+        @bbc_site.bbc_sign_up.enter_year("1990")
+        expect(@bbc_site.bbc_sign_up.get_year_value.length).to eq(4)
+        @bbc_site.bbc_sign_up.submit_button
+      end
+    end
   end
 
   context 'tests on details page' do
@@ -108,7 +108,6 @@ describe "testing the BBC registration journey" do
     end
 
     it "one option should be selected" do
-      # @bbc_site.bbc_sign_up.input_email_spam_choice('optOut')
       expect(@bbc_site.bbc_sign_up.input_email_spam_choice('optOut').selected?).to be false
     end
 
